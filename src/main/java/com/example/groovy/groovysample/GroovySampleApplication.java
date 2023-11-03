640197ab667f772f3e8064cf1653d2a2600b6934
Abdul Mukheed <Abdul.Mukheed@Nwea.org>
2023-11-02 21:11:11 -0700

    Initial commit

---
+++ src/main/java/com/example/groovy/groovysample/GroovySampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.groovy.groovysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovySampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovySampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/groovy/groovysample/GroovySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groovy.groovysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroovySampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
